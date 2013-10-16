0bbf7ff5c0699afaa9cbda97c69f919db50644b0
Sam Johnsson <johnsson.sam@gmail.com>
2013-10-16 13:48:03 +0200

    The files return in a magical way =)

---
+++ Coffee2Project/Coffee2/build/source/buildConfig/debug/com/softwerk/coffee2/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.softwerk.coffee2;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
---
+++ CoffeeProject/Coffee/build/source/buildConfig/debug/se/softwerk/coffee/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package se.softwerk.coffee;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
