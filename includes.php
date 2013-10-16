58c3ec86a87fe6c0316dd3837023ae191b966316
Sam Johnsson <johnsson.sam@gmail.com>
2013-10-16 13:32:54 +0200

    removed more unused files...

--- includes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	require_once(ROOT_PATH.'/classes/helper_functions.php');
-	require_once(ROOT_PATH.'/classes/Ssh2_crontab_manager.php.php');
-	//require_once(ROOT_PATH.'/classes/RestUtils.php');
-	//require_once(ROOT_PATH.'/classes/RestUtils1.php');
-
-
-?>
