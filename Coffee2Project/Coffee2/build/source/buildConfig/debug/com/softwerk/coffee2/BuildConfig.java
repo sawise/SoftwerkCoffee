35ee3024d292ea8f23bec8e7dfe552124ecb7c60
Lucas Viksten <lucas.viksten93@gmail.com>
2013-11-08 15:25:32 +0100

    Removed som files and folders

--- Coffee2Project/Coffee2/build/source/buildConfig/debug/com/softwerk/coffee2/BuildConfig.java
+++
@@ -1,7 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.softwerk.coffee2;
-
-public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
-}
--- Coffee2Project/Coffee2/src/main/java/com/softwerk/coffee2/Helloworld.java
+++
@@ -1,7 +0,0 @@
-package com.softwerk.coffee2;
-
-/**
- * Created by sam on 9/27/13.
- */
-public class Helloworld {
-}
